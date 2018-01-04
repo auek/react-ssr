c3e82650d920328781cfbf0445695dcbc10ca3cb
August Nam-Ki Ek <august.ek90@gmail.com>
2018-01-04 12:19:31 +0100

    adds a lot of boilerplate/setup logic

--- server/src/client/components/Home.js
+++ server/src/client/components/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>I'm the BEST SUPER home component</div>;
+  return (
+    <div>
+      <div>I'm the BEST SUPER home component</div>
+      <button onClick={() => { console.log('clicked button!')}}>Press me!</button>
+    </div>
+  )
 }
 
 export default Home;
